3b14b0831ff54d4c9d44a53c33c2f8853ac8eee6
Illumaria <astankov.dmitry@gmail.com>
2021-05-23 18:00:29 +0300

    Remove HW1 files

--- ml_project/src/entities/split_params.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass, field
-
-
-@dataclass()
-class SplitParams:
-    random_state: int = field(default=4)
-    val_size: float = field(default=0.2)
--- ml_project/src/entities/train_params.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass, field
-
-
-@dataclass()
-class TrainParams:
-    model_type: str = field(default="RandomForestClassifier")
-    random_state: int = field(default=4)
